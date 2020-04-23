3dd4744f434f96f1a64068ccc16bad5b981930de
conorm <conorm456@hotmail.com>
2020-04-23 20:30:16 +0100

    First visual with cubes complete

--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -1,7 +1,10 @@
 package ie.tudublin;
 
-import example.CubeVisual;
-import example.MyVisual;
+import c18312261.MyVisualiser;
+import c18312261.CircleVis;
+//import example.CubeVisual;
+//import example.CubeVisual1;
+//import example.MyVisual;
 
 public class Main
 {	
@@ -9,12 +12,16 @@ public class Main
 	public void startUI()
 	{
 		String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new CubeVisual());		
+		//processing.core.PApplet.runSketch( a, new CubeVisual());
+		//processing.core.PApplet.runSketch( a, new CubeVisual1());
+		//processing.core.PApplet.runSketch( a, new MyVisual());
+		processing.core.PApplet.runSketch( a, new MyVisualiser());
+		processing.core.PApplet.runSketch( a, new CircleVis());	
 	}
 
 	public static void main(String[] args)
 	{
 		Main main = new Main();
-		main.startUI();			
+		main.startUI();	
 	}
 }
