36af85583c2c910b285afd13fa99fce490e44643
conorm <conorm456@hotmail.com>
2020-04-29 18:05:18 +0100

    Circle Visual with working interaction

---
+++ java/src/c18312261/CircleVis.java
@@ -0,0 +1,63 @@
+package c18312261;
+
+import ie.tudublin.*;
+
+public class CircleVis extends Visual
+{
+  
+    int noOfCircles = 1;
+
+    public void settings()
+    {
+        size(1024, 800, P3D);
+    }
+
+    public void setup()
+    {
+        startMinim();
+        startListening();
+        //loadAudio("heroplanet.mp3");
+        colorMode(HSB);
+    }
+
+    public void keyPressed()
+    {
+        if (key == ' ')
+        {
+            getAudioPlayer().cue(0);
+            getAudioPlayer().play();
+        }
+        if (key == '1')
+        {
+            noOfCircles++;
+
+        }
+        
+        //switch statement to change visuals
+    }
+
+    float angle = 0;
+
+    public void draw()
+    {
+        background(0);
+        calculateAverageAmplitude();
+        stroke(map(getSmoothedAmplitude(), 0, 1, 0, 255), 255, 255);
+        strokeWeight(5);
+        noFill();
+        lights();
+        
+        camera(0, 0, 0, 0, 0, -1, 0, 1, 0);
+
+        pushMatrix();
+        translate(0, 0, -200);
+        
+        //creates the specified no of circles
+        for(int i = 1; i < noOfCircles + 1; i++)
+        {
+            circle(0, 0, 60 + ((i * 300) * getSmoothedAmplitude()));
+        }   
+        popMatrix();
+    }
+  
+}
